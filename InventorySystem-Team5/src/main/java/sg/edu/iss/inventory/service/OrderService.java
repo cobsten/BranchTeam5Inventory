aae6bdfd16db9f2a693f47cd5ebf4ba0760027d0
cobsten <cobsten@gmail.com>
2017-12-11 15:26:18 +0800

    commit service/OrderService.java

--- InventorySystem-Team5/src/main/java/sg/edu/iss/inventory/service/OrderService.java
+++ InventorySystem-Team5/src/main/java/sg/edu/iss/inventory/service/OrderService.java
@@ -1,5 +1,21 @@
 package sg.edu.iss.inventory.service;
 
+import java.util.ArrayList;
+
+import sg.edu.iss.inventory.exception.DuplicatePartNumException;
+import sg.edu.iss.inventory.exception.MismatchPartNumException;
+import sg.edu.iss.inventory.model.OrderCartItem;
+import sg.edu.iss.inventory.model.Product;
+import sg.edu.iss.inventory.model.ProductSupplier;
+
 public interface OrderService {
 
+	public ArrayList<OrderCartItem> createToOrderList() throws MismatchPartNumException;
+
+	public void addToOrderList(Product product, ArrayList<OrderCartItem> orderList)
+			throws DuplicatePartNumException, MismatchPartNumException;
+
+	public int qtyLowUnitPrice(OrderCartItem orderCartItem) throws MismatchPartNumException;
+
+	public int computeQty(Product product, ProductSupplier ps) throws MismatchPartNumException;
 }
